5f33b03b2472d5fa813fafe1770d642af045b21a
root <root@ip-172-31-48-31.ec2.internal>
2018-04-06 21:39:52 +0000

    changed kubernetes footer

--- wordpress/wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
+++ wordpress/wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
@@ -10,5 +10,5 @@
 
 ?>
 <div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'Kubernetes1' ); ?></a>
+	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'Kubernetes CTS' ); ?></a>
 </div><!-- .site-info -->
