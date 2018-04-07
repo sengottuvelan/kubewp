8a0e51a8115f742205a3e8925160b8419947dd7c
root <root@ip-172-31-48-31.ec2.internal>
2018-04-07 02:47:00 +0000

    changed the footer to wp

--- wordpress/wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
+++ wordpress/wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
@@ -10,5 +10,5 @@
 
 ?>
 <div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'Kubernetes rock n roll ' ); ?></a>
+	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'Kubernetes wp' ); ?></a>
 </div><!-- .site-info -->
